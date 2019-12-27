6995ebdefe19642201773b8a36a96dfb9ef14cde
dalent2 <59263130+dalent2@users.noreply.github.com>
2019-12-27 13:30:48 +0500

    Updates

---
+++ precache-manifest.03d8a849320df904a84decb24c884595.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fdcf372c8f307ab12bb1cf459728be1",
+    "url": "/GG2/index.html"
+  },
+  {
+    "revision": "fda36a282dc76e25e154",
+    "url": "/GG2/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "05e236983a626adc7121",
+    "url": "/GG2/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "fda36a282dc76e25e154",
+    "url": "/GG2/static/js/2.29869ddd.chunk.js"
+  },
+  {
+    "revision": "05e236983a626adc7121",
+    "url": "/GG2/static/js/main.f2760327.chunk.js"
+  },
+  {
+    "revision": "e8c3b92abbe485b66b3d",
+    "url": "/GG2/static/js/runtime-main.7082c49b.js"
+  },
+  {
+    "revision": "8d7093ec6538e9a9050d6a9ada5176fd",
+    "url": "/GG2/static/media/persik.8d7093ec.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk-apps-currency/precache-manifest.fb168179f52b55526968aae1f6830fb4.js"
+  "/GG2/precache-manifest.03d8a849320df904a84decb24c884595.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vk-apps-currency/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GG2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
