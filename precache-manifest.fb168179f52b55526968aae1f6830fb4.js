96d4a92262e1657f115d39c314a2fbed56a2d88a
dalent2 <59263130+dalent2@users.noreply.github.com>
2019-12-27 11:50:22 +0500

    Updates

---
+++ precache-manifest.fb168179f52b55526968aae1f6830fb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acaafe31c507abb6aa06f6cce34cbc4e",
+    "url": "/vk-apps-currency/index.html"
+  },
+  {
+    "revision": "4c402b450922e858e519",
+    "url": "/vk-apps-currency/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "059c0ef6c250cf7a20c8",
+    "url": "/vk-apps-currency/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "4c402b450922e858e519",
+    "url": "/vk-apps-currency/static/js/2.80b0f97f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/vk-apps-currency/static/js/2.80b0f97f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "059c0ef6c250cf7a20c8",
+    "url": "/vk-apps-currency/static/js/main.91faefa0.chunk.js"
+  },
+  {
+    "revision": "e16e2ca74b332c706071",
+    "url": "/vk-apps-currency/static/js/runtime-main.aef1e424.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-apps-currency/static/media/persik.4e1ec840.png"
+  }
+]);
